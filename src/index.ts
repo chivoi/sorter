ede8ac8f5cf857d465ec3f7f8261866a08f1e42b
Ana Lastoviria <ana.lastoviria@zendesk.com>
2021-11-24 10:00:55 +1100

    add sort of array of numbers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-console.log("Hoi");
+class Sorter {
+    constructor(public collection: number[]){}
 
-const logSmth = () => {
-    console.log("there");
-};
+    sort(): void {
+        const { length } = this.collection;
 
-logSmth();
+        for (let i = 0; i < length; i++){
+            for (let j = 0; j < length - i - 1; j++){
+                // if array of numbers
+                if(this.collection[j] > this.collection[j+1]){
+                    const leftHand = this.collection[j];
+                    this.collection[j]=this.collection[j+1];
+                    this.collection[j+1] = leftHand;
+                }
+
+                // if string
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
