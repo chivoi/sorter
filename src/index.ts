efe0ae88130f0367b264d0b6fc9fb55cd42d9546
Ana Lastoviria <ana.lastoviria@zendesk.com>
2021-11-29 09:06:00 +1100

    add type guards for sort

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 class Sorter {
-    constructor(public collection: number[]){}
+    constructor(public collection: number[] | string){}
 
     sort(): void {
         const { length } = this.collection;
 
         for (let i = 0; i < length; i++){
             for (let j = 0; j < length - i - 1; j++){
-                // if array of numbers
-                if(this.collection[j] > this.collection[j+1]){
-                    const leftHand = this.collection[j];
-                    this.collection[j]=this.collection[j+1];
-                    this.collection[j+1] = leftHand;
+                
+                if (this.collection instanceof Array) {
+                    if(this.collection[j] > this.collection[j+1]){
+                        // I don't like this
+                        const leftHand = this.collection[j];
+                        this.collection[j]=this.collection[j+1];
+                        this.collection[j+1] = leftHand;
+                    }
                 }
 
-                // if string
+                if (typeof this.collection === 'string') {
+                    
+                }
             }
         }
     }
